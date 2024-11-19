3bf4b055c64b0edd786c2709c23c08fcf5d994b7
Elena Semenova <lenzwergy@gmail.com>
2024-11-19 13:24:33 +0300

    changed some css-setting, separated the controllers into two different files, changed some router-settings, but i didn't test the app

---
+++ controllers/controllers_lm.js
@@ -0,0 +1,211 @@
+import DB_GENERAL from "../db_model/db_general.js";
+import updateInfos from "../db_model/db_while_playing.js";
+import startGame from "../db_model/db_start.js";
+
+// MAIN PAGE - LIGHT MODE - DIFFERENT LANGUAGES 
+
+const lm_mainPage = (req, res) => {
+    const lang = req.params.lang;
+    if (lang === 'ru') {
+        res.render('ru/light_mode/start');
+    }
+    if (lang === 'en') {
+        res.render('en/light_mode/start');
+    }
+    if (lang === 'de') {
+        res.render('de/light_mode/start');
+    }
+
+}
+
+// REGISTRATION - LIGHT MODE - DIFFERENT LANGUAGES
+
+const lm_registrationPage = (req, res) => {
+    const lang = req.params.lang;
+    if (lang === 'ru') {
+    res.render('ru/light_mode/registration');
+    }
+    if (lang === 'en') {
+        res.render('en/light_mode/registration');
+    }
+    if (lang === 'de') {
+        res.render('de/light_mode/registration');
+    }
+}
+
+
+// NEW PLAYER - LIGHT MODE - DIFFERENT LANGUAGES
+
+const lm_addNewPlayer = async(req, res) => {
+    let newPlayer = {};
+    let playerID; 
+    console.log('Check if this email is already in the database');
+    const data = req.body;
+    const lang = req.params.lang;
+    try {
+    const isRegistered = await DB_GENERAL.isRegistered(data.email);
+    console.log(isRegistered);
+    if(isRegistered.length === 0) {
+        newPlayer.name = data.playerName;
+        if (!data.phoneNumber) {
+            newPlayer.phone_number = null;
+        }
+        newPlayer.email = data.email;
+        newPlayer.total_questions = 0;
+        newPlayer.right_answers = 0;
+        newPlayer.agreement = data.agreement;
+        playerID = await startGame(newPlayer);
+        return res.redirect(`/game/light-mode/${lang}/questions/${playerID}`);
+    } else {
+        console.log(isRegistered.length);
+        console.log(isRegistered[0].total_questions);
+        if (isRegistered[0].total_questions < 6) {
+            console.log(`Your questions: ${isRegistered[0].total_questions}`);
+            playerID = isRegistered[0].player_id;
+            return res.redirect(`/game/light-mode/${lang}/questions/${playerID}`);
+        } else {
+            console.log(`Your questions: ${isRegistered[0].total_questions}`);
+            playerID = isRegistered[0].player_id;
+            return res.redirect(`/game/light-mode/${lang}/${playerID}/final_result`);
+        }
+    }
+    } catch (error) {
+        console.log('Error while registration: ', error);
+    }
+}
+
+
+// NEW QUESTION - LIGHT MODE - DIFFERENT LANGUAGES
+
+const lm_newQuestion = async(req, res) => {
+    const playerID = req.params.player_id;
+    const lang = req.params.lang;
+    try {
+        const player_data = await DB_GENERAL.aboutPlayer(playerID);
+        console.log(player_data);
+        const randomIndex = (availableQuestions) => {
+            return Math.floor(Math.random() * availableQuestions);
+        };
+        const questions = await DB_GENERAL.availableQuestions(playerID);
+        console.log(questions.length);
+        let questionIndex = randomIndex(questions.length);
+        console.log(questionIndex);
+        const pulledQuestion = questions[questionIndex];
+        console.log(pulledQuestion);
+            if (lang === 'ru') { 
+                return res.render('ru/light_mode/questions', {pulledQuestion, player_data});
+            }
+            if (lang === 'en') {
+                return res.render('en/light_mode/questions', {pulledQuestion, player_data});
+            }
+            if (lang === 'de') {
+                return res.render('de/light_mode/questions', {pulledQuestion, player_data})
+            }
+    }  catch (error) {
+        console.log('You have to deal with: ' + error);
+        return res.status(500).json({errorMessage: 'No data has been given!'});
+    }
+}
+
+
+// BUILD FEEDBACK - LIGHT MODE - DIFFERENT LANGUAGES
+
+const lm_buildFeedback = async(req, res) => {
+
+    // taking player's guess
+
+    const data = req.body;
+    let guess;
+    if (data.guess === 'true') {
+        guess = 1;
+    } else {
+        guess = 0;
+    }
+
+    // taking all the infos to render feedback
+
+
+    const playerID = req.params.player_id;
+    const playerInfo = await DB_GENERAL.aboutPlayer(playerID);
+    const questionID = req.params.question_id;
+    const question = await DB_GENERAL.getQuestion(questionID, playerID);
+    const lang = req.params.lang;
+    
+    let playerTotalScore = playerInfo[0].total_questions;
+    let playerRightScore = playerInfo[0].right_answers;
+
+    if (question[0].company_exists === guess) {
+        console.log('You are right!');
+        playerTotalScore = playerTotalScore + 1;
+        playerRightScore = playerRightScore + 1;
+        const resultOfTransaction = await updateInfos(playerTotalScore, playerRightScore, playerID, questionID);
+        if (resultOfTransaction === "ok") {
+            const newPlayerInfo = await DB_GENERAL.aboutPlayer(playerID);
+            if (lang === 'ru') { 
+                return res.render('ru/light_mode/feedback', {question, newPlayerInfo, guess});
+            }
+            if (lang === 'en') {
+                return res.render('en/light_mode/feedback', {question, newPlayerInfo, guess});
+            }
+            if (lang === 'de') {
+                return res.render('de/light_mode/feedback', {question, newPlayerInfo, guess});
+            }
+        }
+    } else {
+        console.log('You are false!');
+        playerTotalScore = playerTotalScore + 1;
+        const resultOfTransaction = await updateInfos(playerTotalScore, playerRightScore, playerID, questionID);
+        if (resultOfTransaction) {
+            const newPlayerInfo = await DB_GENERAL.aboutPlayer(playerID);
+            if (lang === 'ru') { 
+                return res.render('ru/light_mode/feedback', {question, newPlayerInfo, guess});
+            }
+            if (lang === 'en') {
+                return res.render('en/light_mode/feedback', {question, newPlayerInfo, guess});
+            }
+            if (lang === 'de') {
+                return res.render('de/light_mode/feedback', {question, newPlayerInfo, guess});
+            }
+        }    
+}
+}
+
+// FINAL RESULT - LIGHT MODE - DIFFERENT LANGUAGES
+
+const lm_finalResult = async(req, res) => {
+    const playerID = req.params.player_id;
+    const lang = req.params.lang;
+    try {
+    const playerInfo = await DB_GENERAL.aboutPlayer(playerID);
+    const isTable = await DB_GENERAL.isTable();
+    console.log(isTable.length);
+    for (let i = 0; i < isTable.length; i++) {
+            console.log('Current table: ', isTable[i].Tables_in_petproject_game);
+            if (isTable[i].Tables_in_petproject_game === `questions_for_${playerID}`) {
+            console.log(`There is a table for ${playerInfo[0].name}`);
+            const deleted = await DB_GENERAL.deleteCopies(playerID);
+            console.log(deleted);
+        }
+    }
+        if (lang === 'ru') { 
+            return res.render('ru/light_mode/final_result', { playerInfo });
+        }
+        if (lang === 'en') {
+            return res.render('en/light_mode/final_result', { playerInfo });
+        }
+        if (lang === 'de') {
+            return res.render('de/light_mode/final_result', { playerInfo });
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export default {
+    lm_mainPage,
+    lm_registrationPage,
+    lm_addNewPlayer,
+    lm_newQuestion,
+    lm_buildFeedback,
+    lm_finalResult
+}
--- routes/routes.js
+++ routes/routes.js
@@ -1,21 +1,29 @@
 import express from "express";
 import { Router } from "express";
-import controllers from "../controllers/controllers.js";
+import controllers_dm from "../controllers/controllers_dm.js";
+import controllers_lm from "../controllers/controllers_lm.js";
 
 const router = new Router();
 
-router.get('/', controllers.startRedirect);
-router.get('/:lang', controllers.mainPageLang);
-router.get('/light-mode/:lang', controllers.lightModeMainPage);
-router.get('/:lang/registration', controllers.registrationPage);
-router.get('/light-mode/:lang/registration', controllers.lightModeRegistrationPage);
-router.post('/:lang/registration/newUser', controllers.addNewPlayer);
-router.post('/light-mode/:lang/registration/newUser', controllers.lightModeAddNewPlayer);
-router.get('/:lang/questions/:player_id', controllers.newQuestion);
-router.get('/light-mode/:lang/questions/:player_id', controllers.lightModeNewQuestion);
-router.post('/:lang/questions/:player_id/:question_id/feedback', controllers.buildFeedback);
-router.post('/light-mode/:lang/questions/:player_id/:question_id/feedback', controllers.lightModeBuildFeedback);
-router.get('/:lang/:player_id/final_result', controllers.renderFinalResult);
-router.get('/light-mode/:lang/:player_id/final_result', controllers.lightModeRenderFinalResult);
+// CONTROLLERS - DARK MODE
+
+router.get('/', controllers_dm.startRedirect);
+router.get('/:lang', controllers_dm.dm_mainPage);
+router.get('/:lang/registration', controllers_dm.dm_registrationPage);
+router.post('/:lang/registration/newUser', controllers_dm.dm_addNewPlayer);
+router.get('/:lang/questions/:player_id', controllers_dm.dm_newQuestion);
+router.post('/:lang/questions/:player_id/:question_id/feedback', controllers_dm.dm_buildFeedback);
+router.get('/:lang/:player_id/final_result', controllers_dm.dm_finalResult);
+
+
+// CONTROLLERS - LIGHT MODE
+
+router.get('/light-mode/:lang', controllers_lm.lm_mainPage);
+router.get('/light-mode/:lang/registration', controllers_lm.lm_registrationPage);
+router.post('/light-mode/:lang/registration/newUser', controllers_lm.lm_addNewPlayer);
+router.get('/light-mode/:lang/questions/:player_id', controllers_lm.lm_newQuestion);
+router.post('/light-mode/:lang/questions/:player_id/:question_id/feedback', controllers_lm.lm_buildFeedback);
+router.get('/light-mode/:lang/:player_id/final_result', controllers_lm.lm_finalResult);
+
 
 export default router
